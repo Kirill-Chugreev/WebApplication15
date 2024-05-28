edb76d2eb3f3d4de889856f9049be0c7ac4ec85e
Chugreev Kirill <wern587@gmail.com>
2024-05-28 18:57:43 +0300

    Добавьте файлы проекта.

---
+++ WebApplication15/Program.cs
@@ -0,0 +1,11 @@
+using WebApplication15;
+
+
+var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+
+app.UseMiddleware<ErrorHandlingMiddleware>();
+app.UseMiddleware<AuthenticationMiddleware>();
+app.UseMiddleware<RoutingMiddleware>();
+    
+app.Run();
---
+++ WebApplication15/RoutingMiddleware.cs
@@ -0,0 +1,25 @@
+﻿namespace WebApplication15
+{
+    public class RoutingMiddleware
+    {
+        public RoutingMiddleware(RequestDelegate _)
+        {
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string path = context.Request.Path;
+            if (path == "/admin")
+            {
+                await context.Response.WriteAsync("Admin Page");
+            }
+            else if (path == "/user")
+            {
+                await context.Response.WriteAsync("User Page");
+            }
+            else
+            {
+                context.Response.StatusCode = 404;
+            }
+        }
+    }
+}
